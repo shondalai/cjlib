4d5b6b0af0f13c981cf827ebe7697489962896e6
Shondalai <support@shondalai.com>
2022-05-07 11:02:00 +0530

    * Fixed issue with activation

--- com_cjlib/admin/controllers/license.php
+++ com_cjlib/admin/controllers/license.php
@@ -49,12 +49,12 @@ class CjLibControllerLicense extends FormController {
 		        'query' => $args,
 		        'verify' => false
 		    ]);
-		    $data = $response->getBody();
-		} catch (Exception $e) {
-		    $this->setRedirect('index.php?option=com_cjlib', Text::_('COM_CJLIB_ERROR_WHILE_ACTIVATING_LICENSE') . ' Exception: ' . $e->getMessage());
-		}
-		
-		if( !empty($data) ) {
+		    $data = $response->getBody()->getContents();
+		} catch (\GuzzleHttp\Exception\GuzzleException $e) {
+            $this->setRedirect('index.php?option=com_cjlib', Text::_('COM_CJLIB_ERROR_WHILE_ACTIVATING_LICENSE') . ' Exception: ' . $e->getMessage());
+        }
+
+        if( !empty($data) ) {
 		    $json = json_decode( $data );
 
 		    // If the license is successfully activated, we need to add it to local database
@@ -64,24 +64,24 @@ class CjLibControllerLicense extends FormController {
 		        $params->set('license_key', $license_key);
 		        $params->set('license_email', $license_email);
 		        // var_dump($params); // you can check the values
-		        
+
 		        // saving params to database
 		        $component_id = ComponentHelper::getComponent('com_cjlib')->id;
 		        $table = Table::getInstance('extension');
 		        $table->load($component_id);
 		        $table->bind(array('params' => $params->toString()));
-		        
+
 		        // check for error
 		        if ($table->check() && $table->store()) {
 		            $this->setRedirect('index.php?option=com_cjlib', Text::_('COM_CJLIB_ACTIVATION_SUCCESS'));
 		            return;
 		        }
 		    } else if( !empty( $json->error ) ){
-		        $this->setRedirect('index.php?option=com_cjlib', $json->error);
+		        $this->setRedirect('index.php?option=com_cjlib', $json->error, 'error');
 		        return;
 		    }
 		}
-		
+
 		$this->setRedirect('index.php?option=com_cjlib', Text::_('COM_CJLIB_ERROR_WHILE_ACTIVATING_LICENSE') . " Response: " . $data);
 	}
 	
@@ -91,23 +91,26 @@ class CjLibControllerLicense extends FormController {
 	    if( $params->get('license_key') ) {
 	        
 	        $args = array(
+                'wc-api'        => 'software-api',
 	            'request'       => 'deactivation',
 	            'email'         => $params->get('license_email'),
 	            'license_key'   => $params->get('license_key'),
 	            'instance'      => Uri::getInstance()->root(),
 	            'product_id'  	=> 'all-access-pack'
 	        );
-	        
-	        $base_url = $this->activation_url . '?wc-api=software-api';
-	        $base_url = $base_url . '&' . http_build_query( $args );
-	        
-	        $fopen = ini_set("allow_url_fopen", 1);
-	        $data = file_get_contents( $base_url );
-	        
-	        if($fopen !== false) {
-	            ini_set("allow_url_fopen", $fopen);
-	        }
 
+            $data = null;
+            try {
+                $client = new GuzzleHttp\Client();
+                $response = $client->request('GET', $this->activation_url, [
+                    'query' => $args,
+                    'verify' => false
+                ]);
+                $data = $response->getBody()->getContents();
+            } catch (\GuzzleHttp\Exception\GuzzleException $e) {
+                $this->setRedirect('index.php?option=com_cjlib', Text::_('COM_CJLIB_ERROR_WHILE_DEACTIVATING_LICENSE') . ' Exception: ' . $e->getMessage());
+            }
+	        
 	        if( !empty($data) ) {
 	            $json = json_decode( $data );
 	            
@@ -130,7 +133,10 @@ class CjLibControllerLicense extends FormController {
 	                    $this->setRedirect('index.php?option=com_cjlib', Text::_('COM_CJLIB_DEACTIVATION_SUCCESS'));
 	                    return;
 	                }
-	            }
+	            } else if( !empty( $json->error ) ){
+                    $this->setRedirect('index.php?option=com_cjlib', $json->error, 'error');
+                    return;
+                }
 	        }
 	    }
 	    
