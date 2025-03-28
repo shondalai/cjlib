185a0847bd30af3789f9eec99eb4ccf87bde58aa
Shondalai <support@shondalai.com>
2025-03-28 11:19:52 +0530

    ^ Removed legacy references

--- pkg_script.php
+++ pkg_script.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * @package     corejoomla.administrator
+ * @package     shondalai.administrator
  * @subpackage  plg_cjlib
  *
  * @copyright   Copyright (C) 2009 - 2016 BulaSikku Technologies Private Limited. All rights reserved.
@@ -23,7 +23,7 @@ class pkg_cjlibInstallerScript
 		<tr><td>CjLib Library</td><td>Successfully installed</td></tr>
 		<tr><td>CjLib Socials</td><td>Successfully installed</td></tr>
 		<tr><td>CjLib Package Updater</td><td>Successfully installed</td></tr>
-		<tr><td>CoreJoomla Socials Plugin</td><td>Successfully installed</td></tr>
+		<tr><td>Shondalai Socials Plugin</td><td>Successfully installed</td></tr>
 		</table>
 		<p>Thank you for using Shondalai software. Please add a rating and review at Joomla&reg; Extension Directory.</p>';
 	}
